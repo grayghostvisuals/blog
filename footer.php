4fc867690ad481b9e8d31e6518ba29585149e62b
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-11-27 14:29:38 -0500

    v1.9.37 version flags

--- footer.php
+++ footer.php
@@ -21,7 +21,7 @@
 </div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.min.js"><\/script>')</script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/minified/script-min.js?v1.9.36"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/minified/script-min.js?v1.9.37"></script>
 <script async>
     var _gaq=[ ['_setAccount','UA-27240293-1'],['_trackPageview'],['_trackPageLoadTime'],['_setSiteSpeedSampleRate', 100]];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
