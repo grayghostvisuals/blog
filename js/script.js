91f446e65ace147ae6b1f4f314e86cbde6fdc950
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2013-01-29 13:15:33 -0500

    Added Test Badge and Adjusted headline typography and line height

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 if (Modernizr.svg) {
-    $('.blog-name a').append('<img src="http://static.grayghostvisuals.com/imgblog/blogbadge.svgz" alt="gray ghost visuals press icon leading you home" class="logo"><span class="visuallyhidden" aria-hidden="true"><?php esc_attr( bloginfo( "name" ) ); ?></span>');
+    $('.blog-name a').append('<img src="http://192.168.1.100/wordpress/wp-content/themes/blog.grayghostvisuals/img/testbadge.png" alt="gray ghost visuals press icon leading you home" class="logo"><span class="visuallyhidden" aria-hidden="true"><?php esc_attr( bloginfo( "name" ) ); ?></span>');
 }else {
     $('.blog-name a').append('<img src="http://static.grayghostvisuals.com/imgblog/blogbadge.png" alt="gray ghost visuals press icon leading you home" class="logo"><span class="visuallyhidden" aria-hidden="true"><?php esc_attr( bloginfo( "name" ) ); ?></span>');
 }
