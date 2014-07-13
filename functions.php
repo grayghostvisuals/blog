5db76cab83f780936a84ec2141602c6fd0e1a889
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-07-12 23:27:08 -0400

    Custom Admin Dashboard Snippets

--- functions.php
+++ functions.php
@@ -201,5 +201,142 @@ if ( ! function_exists( 'wpflex_setup' ) ) :
 		//required for theme submission
 		add_action( 'widgets_init' , 'wpflex_widget' );
 
+
+		/*------------------------------------------------------------------------------------------------[ admin login styling ] */
+
+		/**
+		 * function my_login_css
+		 * Custom CSS file for the admin panel and login page
+		 *
+		 * Enqueing isn't really working on the login page; this method
+		 * is more reliable. Hopefully this is fixed soon.
+		 *
+		 * @since 1.0
+		 */
+		function my_login_css() {
+			echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/css/wp-login.css">';
+		}
+		add_action('login_head', 'my_login_css');
+
+		/**
+		 * function my_login_imgurl
+		 * Set the URL to which the login logo image is linked
+		 *
+		 * @since 1.0
+		 */
+		function my_login_imgurl() {
+			return home_url();
+		}
+		add_filter( 'login_headerurl', 'my_login_imgurl');
+
+		/**
+		 * function my_login_imgtitle
+		 * Set the title of the login page
+		 *
+		 * @since 1.0
+		 */
+		function my_login_imgtitle() {
+			return get_bloginfo('title', 'display');
+		}
+		add_filter( 'login_headertitle', 'my_login_imgtitle');
+
+
+		/*------------------------------------------------------------------------------------------------[ admin dashboard ] */
+
+		/**
+		 * function my_remove_dashboard_widgets
+		 * Unset dashboard widgets
+		 *
+		 * @author chrisvanpatten
+		 */
+		function my_remove_dashboard_widgets() {
+			// Left column
+			remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+			//remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+			remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+			//remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+
+			// Right column
+			remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+			remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+			//remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+			remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
+		}
+		add_action('wp_dashboard_setup', 'my_remove_dashboard_widgets' );
+
+		/**
+		 * function my_blog_rss_output
+		 * Build an RSS feed widget
+		 *
+		 * @author chrisvanpatten
+		 */
+		function my_blog_rss_output() {
+			echo '<div class="rss-widget">';
+
+			wp_widget_rss_output( array(
+				'url'          => 'http://blog.grayghostvisuals.com/feed/',
+				'title'        => 'Widget Title',
+				'items'        => 2, // Number of items to display
+				'show_summary' => 1, // Boolean: show article excerpt
+				'show_author'  => 1, // Boolean: show article author
+				'show_date'    => 1, // Boolean: show article date
+			) );
+
+			echo "</div>";
+		}
+
+		/**
+		 * function my_blog_rss_widget
+		 * Display the RSS widget you built above
+		 *
+		 * @author chrisvanpatten
+		 */
+		function my_blog_rss_widget() {
+			add_meta_box( 'my-blog-rss', 'Widget Title', 'my_blog_rss_output', 'dashboard', 'side', 'high' );
+		}
+		add_action('wp_dashboard_setup', 'my_blog_rss_widget');
+
+		/**
+		 * function my_custom_right_now_widget
+		 * Add Custom Post Types and Taxonomies to the 'Right Now' Dashboard Widget
+		 *
+		 * @link http://wpsnipp.com/index.php/functions-php/include-custom-post-types-in-right-now-admin-dashboard-widget/
+		 */
+		function my_custom_right_now_widget() {
+			$args = array(
+				'public'   => true,
+				'_builtin' => false,
+			);
+			$output   = 'object';
+			$operator = 'and';
+
+			$post_types = get_post_types( $args , $output , $operator );
+			foreach( $post_types as $post_type ) {
+				$num_posts = wp_count_posts( $post_type->name );
+				$num       = number_format_i18n( $num_posts->publish );
+				$text      = _n( $post_type->labels->singular_name, $post_type->labels->name , intval( $num_posts->publish ) );
+				if ( current_user_can( 'edit_posts' ) ) {
+					$num  = "<a href = 'edit.php?post_type = $post_type->name'>$num</a>";
+					$text = "<a href = 'edit.php?post_type = $post_type->name'>$text</a>";
+				}
+				echo '<tr><td class="first b b-' . $post_type->name . '">' . $num . '</td>';
+				echo '<td class="t ' . $post_type->name . '">' . $text . '</td></tr>';
+			}
+
+			$taxonomies = get_taxonomies( $args , $output , $operator ); 
+			foreach( $taxonomies as $taxonomy ) {
+				$num_terms = wp_count_terms( $taxonomy->name );
+				$num       = number_format_i18n( $num_terms );
+				$text      = _n( $taxonomy->labels->singular_name, $taxonomy->labels->name , intval( $num_terms ));
+				if ( current_user_can( 'manage_categories' ) ) {
+					$num  = "<a href = 'edit-tags.php?taxonomy = $taxonomy->name'>$num</a>";
+					$text = "<a href = 'edit-tags.php?taxonomy = $taxonomy->name'>$text</a>";
+				}
+				echo '<tr><td class="first b b-' . $taxonomy->name . '">' . $num . '</td>';
+				echo '<td class="t ' . $taxonomy->name . '">' . $text . '</td></tr>';
+			}
+		}
+		add_action( 'right_now_content_table_end' , 'my_custom_right_now_widget' );
+
 	}//end themename_setup
 endif;//end ! function_exists( 'wpflex_setup' )
