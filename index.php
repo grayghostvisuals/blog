fb77be8fc9ad617174a8916620ca497313f0f32b
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-07-23 12:01:34 -0400

    Adjusted class name for entry titles

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 			<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
 				<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 					<header>
-						<h1 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+						<h1 class="entry__title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
 						<?php get_template_part( 'inc/meta' ); ?>
 					</header>
 
