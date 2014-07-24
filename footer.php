ffa2a5f449bef175c88472b1b23636d1eba33e0b
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-07-23 21:06:54 -0400

    Footer Adjustments, Blockquotes and Stuff

--- footer.php
+++ footer.php
@@ -1,8 +1,4 @@
 <footer role="contentinfo">
-	<div class="widgets-wrap">
-		<?php get_sidebar('footer'); ?>
-	</div>
-
 	<ul id="info" class="socials">
 		<li class="twitter"><a href="//twitter.com/#!/gryghostvisuals" class="ss-icon ss-social-circle" aria-hidden="true">twitter</a></li>
 		<li class="github"><a href="//github.com/grayghostvisuals" class="ss-icon ss-social-circle" aria-hidden="true">github</a></li>
@@ -10,6 +6,10 @@
 		<li class="facebook"><a href="//facebook.com/grayghostvisuals" class="ss-icon ss-social-circle" aria-hidden="true">facebook</a></li>
 		<li class="gplus"><a href="//plus.google.com/u/0/109644357599758733825/about" class="ss-icon ss-social-circle" aria-hidden="true">googleplus</a></li>
 	</ul>
+
+	<div class="widgets-wrap">
+		<?php get_sidebar('footer'); ?>
+	</div>
 
 	<small class="copyright">Gray Ghost Visuals Press &bull; Exploring Front&ndash;end Web Development &amp; Design Since <time datetime="2009">2009</time></small>
 </footer>
