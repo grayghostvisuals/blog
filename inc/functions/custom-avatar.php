57709d1aadba8cfbd20cb4fb3d7517ed1f5fd611
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-09-09 23:45:53 -0400

    Custom Avatar Snippet Fix

--- inc/functions/custom-avatar.php
+++ inc/functions/custom-avatar.php
@@ -1,9 +1,9 @@
 <?php
-	function custom_avatar($avatar_defaults) {
-		$custom_avatar_src = 'http://api.adorable.io/avatar/285/abott@adorable.io.png';
-		$custom_avatar_defaults[$custom_avatar_src] = "Adorable Creature";
-		return $custom_avatar_defaults;
-	}
+function custom_avatar($avatar_defaults) {
+	$myavatar = 'http://api.adorable.io/avatar/285/abott@adorable.io.png';
+	$avatar_defaults[$myavatar] = "Adorable Creature";
+	return $avatar_defaults;
+}
 
-	add_filter( 'avatar_defaults', 'custom_avatar' );
+add_filter( 'avatar_defaults', 'custom_avatar' );
 ?>
