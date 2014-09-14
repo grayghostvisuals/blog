d3a6242b0a5839e4790648c52166108721c7ee9f
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-09-14 11:37:20 -0400

    Perf Improvements

--- inc/functions/theme-support.php
+++ inc/functions/theme-support.php
@@ -10,26 +10,27 @@ add_theme_support( 'post-thumbnails', array( 'post', 'page', 'movie' ) );
 // http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
 // Output valid HTML5 for search form, comment form, and comments/
 add_theme_support( 'html5', array(
-    'comment-list',
-    'comment-form',
-    'search-form',
-    'gallery',
-    'caption'
+	'comment-list',
+	'comment-form',
+	'search-form',
+	'gallery',
+	'caption'
 ));
 
+
 $custom_header_args = array(
-    'default-image'          => '',
-    'random-default'         => false,
-    'width'                  => 0,
-    'height'                 => 0,
-    'flex-height'            => false,
-    'flex-width'             => false,
-    'default-text-color'     => '',
-    'header-text'            => true,
-    'uploads'                => true,
-    'wp-head-callback'       => '',
-    'admin-head-callback'    => '',
-    'admin-preview-callback' => '',
+	'default-image'          => '',
+	'random-default'         => false,
+	'width'                  => 0,
+	'height'                 => 0,
+	'flex-height'            => false,
+	'flex-width'             => false,
+	'default-text-color'     => '',
+	'header-text'            => true,
+	'uploads'                => true,
+	'wp-head-callback'       => '',
+	'admin-head-callback'    => '',
+	'admin-preview-callback' => '',
 );
 add_theme_support( 'custom-header', $custom_header_args );
 ?>
--- inc/profile.php
+++ inc/profile.php
@@ -1,3 +1,4 @@
+<?php if ( get_the_author_meta('ID') ) : ?>
 <div class="profile">
 	<div class="profile__avatar">
 		<?php echo get_avatar( get_the_author_meta('ID'), 180 ); ?>
@@ -6,8 +7,9 @@
 	<div class="profile__meta">
 		<h3 class="profile-name"><?php the_author(); ?></h3>
 
-		<div class="profile-desc">
-			<?php the_author_meta('description'); ?>
-		</div>
+		<?php if ( get_the_author_meta('description') ) : ?>
+		<div class="profile-desc"><?php the_author_meta('description'); ?></div>
+		<?php endif; ?>
 	</div>
 </div>
+<?php endif; ?>
