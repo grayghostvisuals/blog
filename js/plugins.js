f309e3bad7dc68c295737a608f23f206a4397aed
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-10-10 14:35:40 -0400

    Lemanz Link and Sticky Scroll Ad

--- js/plugins.js
+++ js/plugins.js
@@ -14,6 +14,35 @@ if (!(window.console && console.log)) {
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
+// http://www.paulirish.com/2009/throttled-smartresize-jquery-event-handler
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
 /*!
 * FitVids 1.0
 *
--- js/script.js
+++ js/script.js
@@ -1,32 +1,95 @@
 $(document).ready(function () {
 
-	$('.flex-video').fitVids();
+	(function (window, document, undefined) {
 
-	var $utility_toggle = $('.utility-bar__toggle'),
-		state = 'active';
+		'use strict';
 
-	function navToggle(event, cname) {
-		event.preventDefault();
-		$('.utility-bar').toggleClass(cname);
-		$('.utility-bar__items').toggleClass(cname);
-	}
+		// Selectors
+		// ==============================================================================
 
-	$utility_toggle.on('click', function(event) {
-		navToggle.call(this, event, state);
-	});
+		var $utility_toggle = $('.utility-bar__toggle'),
+				state           = 'active',
+				hero_height     = $('.branding').height(),
+				content_pad     = $('#content').css('padding-top').split('px')[0],
+				sticky_trigger  = +hero_height + +content_pad,
+				sticky_width    = 1180,
+				fix_class       = 'is-fixed';
 
-	$utility_toggle.on('touchstart', function(event) {
-		navToggle.call(this, event, state);
-	});
+		// Methods
+		// ==============================================================================
+
+		function navToggle(event, cname) {
+			event.preventDefault();
+			$('.utility-bar').toggleClass(cname);
+			$('.utility-bar__items').toggleClass(cname);
+		}
+
+		function stickyAd() {
+			if($(window).width() >= sticky_width) {
+				var fusion_ad       = $('#fusionads'),
+						content_offset  = $('#content').offset().top,
+						scroll_top      = $(window).scrollTop();
+
+				if(scroll_top >= content_offset) {
+					fusion_ad.addClass(fix_class);
+				}
+
+				if(scroll_top < sticky_trigger) {
+					fusion_ad.removeClass(fix_class);
+				}
+			}
+		}
+
+		function stickyResize() {
+			if($(window).width() < sticky_width) {
+				fusion_ad.removeClass(fix_class);
+			}
+			if($(window).width() >= sticky_width) {
+				$(window).on('scroll', stickyAd);
+			}
+		}
+
+		function lemanzSimpleLink(event) {
+			$("html, body").animate({
+				scrollTop: $($.attr(this, 'href')).offset().top
+			}, 1200);
+
+			event.preventDefault();
+		}
 
 
-	$('.formatting-toggle').bind('click', function(event) {
-		event.preventDefault();
-		$(this).next().toggleClass('active');
-	});
+		// Event Handlers
+		// ==============================================================================
+		
+		if(window.matchMedia( "(min-width:" + sticky_width + "px)" ).matches) {
+			$(window).on('scroll', stickyAd);
+		}
 
 
-	$('#article-links li').each(function() {
-		$(this).prepend('<b class="ss-icon">&#x1F517;</b>');
-	});
+		$(window).smartresize(stickyResize);
+
+
+		$('.flex-video').fitVids();
+
+
+		$utility_toggle.on('click', function(event) {
+			navToggle.call(this, event, state);
+		});
+
+
+		$utility_toggle.on('touchstart', function(event) {
+			navToggle.call(this, event, state);
+		});
+
+
+		$('.formatting-toggle').bind('click', function(event) {
+			event.preventDefault();
+			$(this).next().toggleClass('active');
+		});
+
+		$('.lemanz-sl').on('click', lemanzSimpleLink);
+		$('.lemanz-sl').on('touchstart', lemanzSimpleLink);
+
+	})(window, document);
+
 });
