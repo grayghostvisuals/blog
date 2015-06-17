6a47ed72729024574beb9c7c9f8c58b0275bac17
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-06-17 14:28:35 -0400

    restyling header and oc nav

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form id="search-form" action="<?php echo home_url(); ?>" method="get" role="search">
-    <label for="s">Search the Archives</label>
-    <input type="search" value="<?php esc_attr( the_search_query() ); ?>" name="s" placeholder="enter search term">
+    <label for="s" class="visuallyhidden">Search the Archives</label>
+    <input type="search" value="<?php esc_attr( the_search_query() ); ?>" name="s" placeholder="Search Archives">
 </form>
